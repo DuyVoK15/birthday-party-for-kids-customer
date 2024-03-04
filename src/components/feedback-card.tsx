3f30a61f5f05259148fffa67c9f7e286f98428ab
DuyVoK15 <duybpz@gmail.com>
2024-03-04 17:04:18 +0700

    fix: last fix for deploy

--- src/components/feedback-card.tsx
+++ src/components/feedback-card.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-import { Typography, Card, CardBody, Avatar, Rating } from "@material-tailwind/react";
+import {
+  Typography,
+  Card,
+  CardBody,
+  Avatar,
+  Rating,
+} from "@material-tailwind/react";
 
 interface FeedbackCardProps {
   img: string;
@@ -9,28 +15,41 @@ interface FeedbackCardProps {
   title: string;
 }
 
-export function FeedbackCard({ img, feedback, client, title }: FeedbackCardProps) {
+export function FeedbackCard({
+  img,
+  feedback,
+  client,
+  title,
+}: FeedbackCardProps) {
   return (
-    <Card placeholder=""  shadow={false} className="items-start text-left">
-      <CardBody>
-        <Avatar placeholder=""  src={img} className="mb-2" alt={client} size="xl" />
-        <Typography placeholder=""  variant="h6" color="blue-gray">
+    <Card placeholder="" shadow={false} className="items-start text-left">
+      <CardBody placeholder="">
+        <Avatar
+          placeholder=""
+          src={img}
+          className="mb-2"
+          alt={client}
+          size="xl"
+        />
+        <Typography placeholder="" variant="h6" color="blue-gray">
           {client}
         </Typography>
-        <Typography placeholder="" 
+        <Typography
+          placeholder=""
           variant="small"
           color="blue-gray"
-          className="mt-1 mb-5 block font-normal"
+          className="mb-5 mt-1 block font-normal"
         >
           {title}
         </Typography>
-        <Typography placeholder="" 
+        <Typography
+          placeholder=""
           variant="paragraph"
           className="mb-6 font-normal text-gray-500"
         >
           &quot;{feedback}&quot;
         </Typography>
-        <Rating  placeholder=""  value={5} readonly />
+        <Rating placeholder="" value={5} readonly />
       </CardBody>
     </Card>
   );
