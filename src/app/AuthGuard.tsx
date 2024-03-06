4ce2de14bbcd7caab76f5ce210464a4e8c80c99f
DuyVoK15 <duybpz@gmail.com>
2024-03-06 12:43:12 +0700

    fix: fix components

--- src/app/AuthGuard.tsx
+++ src/app/AuthGuard.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { APP_CONSTANTS } from "@/enums/app";
 import { setPathName } from "@/lib/features/app.slice";
-import { useAppDispatch } from "@/lib/hooks";
-import { redirect, usePathname } from "next/navigation";
+import { loadAuthState } from "@/lib/features/auth.slice";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { redirect, usePathname, useRouter } from "next/navigation";
 import React, { FC, ReactNode, useEffect } from "react";
 
 interface AuthGuardProps {
@@ -10,19 +11,28 @@ interface AuthGuardProps {
 }
 const AuthGuard: FC<AuthGuardProps> = ({ children }) => {
   const pathName = usePathname();
+  const router = useRouter();
   const dispatch = useAppDispatch();
-  const isAuthenticated = Boolean(
-    localStorage.getItem(APP_CONSTANTS.ACCESS_TOKEN),
-  );
+  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
+  useEffect(() => {
+    console.log("vô loadAuthState AuthGuard");
+    const fetchLoadAuthState = async () => {
+      await dispatch(loadAuthState());
+    };
+    fetchLoadAuthState();
+  }, []);
+  console.log("vô AuthGuard");
   useEffect(() => {
     const fetchPathName = async () => {
       await dispatch(setPathName(pathName));
     };
     fetchPathName();
-    if (!isAuthenticated) {
-      redirect("/authen");
-    }
   }, [isAuthenticated]);
+
+  // if (!isAuthenticated) {
+  //   router.push("/authen");
+  // } 
+  
   return <>{children}</>;
 };
 
