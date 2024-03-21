ea6eabca9bbca685e4445458a2115e939e094f05
DuyVoK15 <duybpz@gmail.com>
2024-03-21 21:52:10 +0700

    fix: fix for deploy

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,4 @@
-
-export * from "./out-impressive-stats";
 export * from "./hero";
 export * from "./layout";
 export * from "./page";
-export * from "./students-feedback";
 export * from "./package-cards";
-export * from "./testimonial";
-export * from "./courses-categories";
-export * from "./events";
-
-
