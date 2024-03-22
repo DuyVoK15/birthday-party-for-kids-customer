60ef097ceb4d15af5741822325510e9f08c9ef48
DuyVoK15 <duybpz@gmail.com>
2024-03-22 19:14:04 +0700

    feat: UI booking history, detail

--- src/app/booking-history/page.tsx
+++ src/app/booking-history/page.tsx
@@ -1,62 +1,41 @@
-"use client";;
+"use client";
+import { CalendarOutlined, EnvironmentOutlined } from "@ant-design/icons";
+import { Avatar, Button, Divider, Flex, Image, Space, Typography } from "antd";
 import * as React from "react";
 
-const steps = [
-  "Select campaign settings",
-  "Create an ad group",
-  "Create an ad",
-  "Create an ad group",
-  "Create an ad",
-];
-
-export default function HorizontalLinearStepper() {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set<number>());
-
-  const isStepOptional = (step: number) => {
-    return step === 1;
-  };
-
-  const isStepSkipped = (step: number) => {
-    return skipped.has(step);
-  };
-
-  const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
+const Item = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string | React.ReactNode;
+}) => {
   return (
-    <div style={{marginRight: 150, marginLeft: 150}}>
+    <Flex gap={100}>
+      <div className="flex-1 font-bold">{title}</div>
+      <div className="flex-1 text-sm font-normal text-gray-700">
+        {description}
+      </div>
+    </Flex>
+  );
+};
+const ChildItem = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string | React.ReactNode;
+}) => {
+  return (
+    <Flex gap={100} justify="space-between">
+      <div className="font-bold text-black">{title}</div>
+      <div className="text-sm font-normal text-gray-700">{description}</div>
+    </Flex>
+  );
+};
+export default function BookingHistory() {
+  return (
+    <div className="container mx-auto mt-10">
       
     </div>
   );
